65099f5ea3d17b73c8b3874b83a912255f4cf830
shahnavaz <shekhshahnavaz.a@gmail.com>
2022-10-05 20:21:07 +0530

    example

---
+++ app/src/main/java/com/example/foodverse/welcome_screen.java
@@ -0,0 +1,16 @@
+package com.example.foodverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class welcome_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+
+
+}
